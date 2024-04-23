f2692739f5280de6d685df78db012ddfd6bea6c7
Estephe <estephefath@gmail.com>
2024-04-23 10:08:03 +0200

    add minimum security on the password

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthGuard } from '../auth.guard';
+import { UserService } from '../services/user.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -9,8 +11,16 @@ import { AuthGuard } from '../auth.guard';
 })
 export class UserComponent implements OnInit {
 
+  userName: string = "";
+  email: string = "";
+  password: string = "";
+
   constructor(
-    private authGuard: AuthGuard) { }
+    private authGuard: AuthGuard,
+    private userService: UserService,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
   }
