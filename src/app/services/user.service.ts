b5b05bc2046a6f674893a5f77b3535e868db7655
Estephe <estephefath@gmail.com>
2024-04-23 10:17:07 +0200

    revert changes made in user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, Observable, tap } from 'rxjs';
-import { User, UserForm } from '../types';
 
 @Injectable({
   providedIn: 'root'
@@ -18,21 +16,4 @@ export class UserService {
       return null;
     }
   }
-
-  getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(`/user`).pipe(tap(users => console.log(users)));
-  }
-
-  // Vérification pour l'email de l'utilisateur (voir s'il exsite déjà en base au moment de la création de compte)
-  emailExists(email: string): Observable<boolean> {
-    return this.http.get<User[]>('/user').pipe(
-      map(users => users.some(user => user.email === email))
-    );
-  }
-  
-  async addUser(user: UserForm) {
-    const newUser = await this.http.post<User>("/user", user).toPromise();
-    if (!newUser) throw new Error("Projet non créé.");
-  }
-
 }
